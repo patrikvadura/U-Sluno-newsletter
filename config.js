e93da94222e4b66fab9e60d7db1175a1428a9b5f
Patrik Vaďura <patrikvadura@gmail.com>
2020-07-14 11:29:15 +0200

    first push

---
+++ config.js
@@ -0,0 +1,29 @@
+/*
+|-------------------------------------------------------------------------------
+| Development config               https://maizzle.com/docs/environments/#local
+|-------------------------------------------------------------------------------
+|
+| The exported object contains the default Maizzle settings for development.
+| This is used when you run the `maizzle build` or `maizzle serve` and it
+| has the fastest build time, since most transformations are disabled.
+|
+*/
+
+module.exports = {
+  build: {
+    assets: {
+      source: 'src/assets/images',
+      destination: 'images',
+    },
+    destination: {
+      path: 'build_local',
+    },
+    tailwind: {
+      css: 'src/assets/css/main.css',
+    },
+    templates: {
+      root: 'src/templates',
+    },
+    googleFonts: 'Open+Sans:wght@300;400;600;700;800&display=swap',
+  },
+}
